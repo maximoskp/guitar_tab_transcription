fb727524632ed943c4dd1fef0031f2ab7d29c5ec
maximoskp <maximoskalpap@gmail.com>
2021-10-30 16:06:59 +0300

    plotting corrections

---
+++ flat_tab_in_CNN/make_figs_tab_flat_CNN_out.py
@@ -0,0 +1,52 @@
+import sys
+if sys.version_info >= (3,8):
+    import pickle
+else:
+    import pickle5 as pickle
+import tensorflow as tf
+from tensorflow import keras
+import os
+import matplotlib.pyplot as plt
+
+with open('..' + os.sep + 'data' + os.sep + 'flat_tablature_dataset.pickle', 'rb') as handle:
+    d = pickle.load(handle)
+
+x_train = d['x_train'].T
+y_train = d['y_train'].T
+x_valid = d['x_valid'].T
+y_valid = d['y_valid'].T
+x_test = d['x_test'].T
+y_test = d['y_test'].T
+
+# load model
+model = keras.models.load_model( 'models/tab_flat_CNN_out/tab_flat_CNN_out_current_best.hdf5' )
+
+model.evaluate( x_test, y_test )
+
+# %%
+sessions_num = 10
+session_ids = np.random.choice(y_test.shape[0]-l, sessions_num, replace=False)
+
+frames_per_session = 10
+
+for i in session_ids:
+    for j in range(frames_per_session):
+        y_pred = model.predict( [x_test[i+j:i+j+1]] )
+        print('predicted: ' + repr(y_pred))
+        print('actual: ' + repr(y_test[i+j]))
+        print('input: ' + repr( np.where(x_test[i+j, :128] != 0 ) ))
+        
+        plt.clf()
+        plt.subplot(3,1,1)
+        # plt.bar( np.arange(y_pred.shape[1]) , y_pred[0] )
+        plt.imshow( np.reshape( y_pred[0] , [6, 25]) , cmap='gray_r' )
+        plt.title('predicted')
+        plt.subplot(3,1,2)
+        # plt.bar( np.arange(y_pred.shape[1]) , y_test[i] )
+        plt.imshow( np.reshape( y_test[i+j] , [6, 25]) , cmap='gray_r' )
+        plt.title('actual')
+        plt.subplot(3,1,3)
+        plt.bar( np.arange(128) , x_test[i+j:i+j+1, :128][0] )
+        plt.title('input')
+        os.makedirs( '../figs/tab_flat_CNN_out/session_'+str(i), exist_ok=True )
+        plt.savefig( '../figs/tab_flat_CNN_out/session_'+str(i)+'/fig_'+str(j)+'.png', dpi=300 )
--- flat_tab_in_CNN/train_tab_flat_CNN_out.py
+++ flat_tab_in_CNN/train_tab_flat_CNN_out.py
@@ -15,11 +15,12 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 import matplotlib.pyplot as plt
+sys.path.insert(1, '..')
 import data_utils
 
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-with open('data' + os.sep + 'flat_tablature_dataset.pickle', 'rb') as handle:
+with open('..' + os.sep + 'data' + os.sep + 'flat_tablature_dataset.pickle', 'rb') as handle:
     d = pickle.load(handle)
 
 x_train = d['x_train'].T
@@ -63,15 +64,17 @@ model.add(out_layer)
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['cosine_similarity'])
 model.summary()
 
+os.makedirs( 'models/tab_flat_CNN_out', exist_ok=True )
+
 # %% 
-filepath = 'data/models/bestLossANN_epoch{epoch:02d}_valLoss{val_loss:.6f}.hdf5'
+filepath = 'models/tab_flat_CNN_out/tab_flat_CNN_out_epoch{epoch:02d}_valLoss{val_loss:.6f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=filepath,
                             monitor='val_loss',
                             verbose=1,
                             save_best_only=True,
                             mode='min')
 
-filepath_current_best = 'data/models/bestLossANN_current_best.hdf5'
+filepath_current_best = 'models/tab_flat_CNN_out/tab_flat_CNN_out_current_best.hdf5'
 checkpoint_current_best = ModelCheckpoint(filepath=filepath_current_best,
                             monitor='val_loss',
                             verbose=1,
@@ -81,31 +84,34 @@ checkpoint_current_best = ModelCheckpoint(filepath=filepath_current_best,
 history = model.fit( x_train, y_train, epochs=1000, batch_size=16,
                     validation_data=(x_valid,y_valid), callbacks=[checkpoint, checkpoint_current_best])
 
-model.save('models/tab_flat_ANN.h5')
+# model.save('models/tab_flat_ANN.h5')
 
-model.evaluate( x_test, y_test )
+# model.evaluate( x_test, y_test )
 
-# %%
-l = 10
-i = np.random.randint(y_test.shape[0]-l)
+# # %%
+# sessions_num = 10
+# session_ids = np.random.choice(y_test.shape[0]-l, sessions_num, replace=False)
 
-for j in range(l):
-    y_pred = model.predict( [x_test[i+j:i+j+1]] )
-    print('predicted: ' + repr(y_pred))
-    print('actual: ' + repr(y_test[i+j]))
-    print('input: ' + repr( np.where(x_test[i+j, :128] != 0 ) ))
-    
-    plt.clf()
-    plt.subplot(3,1,1)
-    # plt.bar( np.arange(y_pred.shape[1]) , y_pred[0] )
-    plt.imshow( np.reshape( y_pred[0] , [6, 25]) , cmap='gray_r' )
-    plt.title('predicted')
-    plt.subplot(3,1,2)
-    # plt.bar( np.arange(y_pred.shape[1]) , y_test[i] )
-    plt.imshow( np.reshape( y_test[i+j] , [6, 25]) , cmap='gray_r' )
-    plt.title('actual')
-    plt.subplot(3,1,3)
-    plt.bar( np.arange(128) , x_test[i+j:i+j+1, :128][0] )
-    plt.title('input')
-    os.makedirs( 'figs/session_'+str(i), exist_ok=True )
-    plt.savefig( 'figs/session_'+str(i)+'/fig_'+str(j)+'.png', dpi=300 )
+# frames_per_session = 10
+
+# for i in session_ids:
+#     for j in range(frames_per_session):
+#         y_pred = model.predict( [x_test[i+j:i+j+1]] )
+#         print('predicted: ' + repr(y_pred))
+#         print('actual: ' + repr(y_test[i+j]))
+#         print('input: ' + repr( np.where(x_test[i+j, :128] != 0 ) ))
+        
+#         plt.clf()
+#         plt.subplot(3,1,1)
+#         # plt.bar( np.arange(y_pred.shape[1]) , y_pred[0] )
+#         plt.imshow( np.reshape( y_pred[0] , [6, 25]) , cmap='gray_r' )
+#         plt.title('predicted')
+#         plt.subplot(3,1,2)
+#         # plt.bar( np.arange(y_pred.shape[1]) , y_test[i] )
+#         plt.imshow( np.reshape( y_test[i+j] , [6, 25]) , cmap='gray_r' )
+#         plt.title('actual')
+#         plt.subplot(3,1,3)
+#         plt.bar( np.arange(128) , x_test[i+j:i+j+1, :128][0] )
+#         plt.title('input')
+#         os.makedirs( 'figs/tab_flat_CNN_out/session_'+str(i), exist_ok=True )
+#         plt.savefig( 'figs/tab_flat_CNN_out/session_'+str(i)+'/fig_'+str(j)+'.png', dpi=300 )
