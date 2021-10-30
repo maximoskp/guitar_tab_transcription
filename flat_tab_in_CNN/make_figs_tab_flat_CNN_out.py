9cb4fd95961f7a631b4e1d0ca61e1f60f9515057
maximoskalpap <maximos@athenarc.gr>
2021-10-30 22:59:47 +0300

    added decision to test figs

--- flat_tab_in_CNN/make_figs_tab_flat_CNN_out.py
+++ flat_tab_in_CNN/make_figs_tab_flat_CNN_out.py
@@ -8,6 +8,7 @@ from tensorflow import keras
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import midi2tab_utils as m2t
 
 with open('..' + os.sep + 'data' + os.sep + 'flat_tablature_dataset.pickle', 'rb') as handle:
     d = pickle.load(handle)
@@ -22,7 +23,7 @@ y_test = d['y_test'].T
 # load model
 model = keras.models.load_model( '../models/tab_flat_CNN_out/tab_flat_CNN_out_current_best.hdf5' )
 
-model.evaluate( x_test, y_test )
+# model.evaluate( x_test, y_test )
 
 # %%
 sessions_num = 10
@@ -32,21 +33,27 @@ session_ids = np.random.choice(y_test.shape[0]-frames_per_session, sessions_num,
 for i in session_ids:
     for j in range(frames_per_session):
         y_pred = model.predict( [x_test[i+j:i+j+1]] )
-        print('predicted: ' + repr(y_pred))
-        print('actual: ' + repr(y_test[i+j]))
-        print('input: ' + repr( np.where(x_test[i+j, :128] != 0 ) ))
+        midi = x_test[i+j:i+j+1, :128]
+        decision = m2t.midi_and_flat_tab_decision(midi[0], y_pred[0])
+        # print('predicted: ' + repr(y_pred))
+        # print('actual: ' + repr(y_test[i+j]))
+        # print('input: ' + repr( np.where(x_test[i+j, :128] != 0 ) ))
         
         plt.clf()
         plt.subplot(3,1,1)
         # plt.bar( np.arange(y_pred.shape[1]) , y_pred[0] )
         plt.imshow( np.reshape( y_pred[0] , [6, 25]) , cmap='gray_r' )
-        plt.title('predicted')
+        plt.title('probabilities')
         plt.subplot(3,1,2)
+        # plt.bar( np.arange(y_pred.shape[1]) , y_pred[0] )
+        plt.imshow( decision , cmap='gray_r' )
+        plt.title('decision')
+        plt.subplot(3,1,3)
         # plt.bar( np.arange(y_pred.shape[1]) , y_test[i] )
         plt.imshow( np.reshape( y_test[i+j] , [6, 25]) , cmap='gray_r' )
-        plt.title('actual')
-        plt.subplot(3,1,3)
-        plt.bar( np.arange(128) , x_test[i+j:i+j+1, :128][0] )
-        plt.title('input')
+        plt.title('actual: ' + repr(list(np.where(midi[0])[0])))
+        # plt.subplot(2,2,4)
+        # plt.bar( np.arange(128) , x_test[i+j:i+j+1, :128][0] )
+        # plt.title('input')
         os.makedirs( '../figs/tab_flat_CNN_out/session_'+str(i), exist_ok=True )
         plt.savefig( '../figs/tab_flat_CNN_out/session_'+str(i)+'/fig_'+str(j)+'.png', dpi=300 )
---
+++ flat_tab_in_CNN/midi2tab_utils.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 21:16:17 2021
+
+@author: user
+"""
+import numpy as np
+import itertools
+
+def get_midi_full_fretboard():
+    proper_tunning = np.array( [64, 59, 55, 50, 45, 40] )
+    midi_full_fretboard = np.zeros( (6,25) )
+    for i in range(25):
+        midi_full_fretboard[:,i] = proper_tunning + i
+    return midi_full_fretboard
+# end get_midi_full_fretboard
+
+def make_all_binary_tabs_for_binary_midi(m):
+    midi_notes = np.where( m )[0]
+    # print('midi_notes: ', midi_notes)
+    strings_for_notes = []
+    frets_for_notes = []
+    f = get_midi_full_fretboard()
+    for n in midi_notes:
+        tmp_where = np.where( n == f )
+        # print('tmp_where: ', tmp_where)
+        strings_for_notes.append( tmp_where[0] )
+        frets_for_notes.append( tmp_where[1] )
+    # get all combinations of strings
+    # print('strings_for_notes: ', strings_for_notes)
+    all_combinatios = list( itertools.product( *strings_for_notes ) )
+    # print('all_combinatios: ', all_combinatios)
+    # keep only combinations with different elements
+    combinations2keep = []
+    for c in all_combinatios:
+        # check for dublicates
+        if len( list( set( c ) ) ) == len( c ):
+            combinations2keep.append( list(c) )
+    # construct binary fretboard for each string combination
+    # print('combinations2keep: ', combinations2keep)
+    all_binary_fretboards = []
+    for c in combinations2keep:
+        b = np.zeros( (6,25) )
+        # get fret for each string
+        for i,string in enumerate(c):
+            fret_idx = np.where(string == strings_for_notes[i])[0][0].astype(int)
+            fret = frets_for_notes[i][fret_idx]
+            b[ string, fret ] = 1
+        all_binary_fretboards.append( b )
+    return all_binary_fretboards
+# end make_all_binary_tabs_for_binary_midi
+
+def midi_and_flat_tab_decision(m, t):
+    # m: 128x1: midi array
+    # t: 150x1: tab probabilities
+    # print('m: ', m)
+    # print('t: ', t)
+    t_full = np.reshape( t , [6,25] )
+    all_binary_tabs = make_all_binary_tabs_for_binary_midi( m )
+    print('all_binary_tabs: ', all_binary_tabs)
+    all_probs = np.zeros( len(all_binary_tabs) )
+    for i,b in enumerate( all_binary_tabs ):
+        all_probs[i] = np.sum( b*t_full )
+    return all_binary_tabs[ np.argmax( all_probs ) ]
+# end midi_and_flat_tab_decision
