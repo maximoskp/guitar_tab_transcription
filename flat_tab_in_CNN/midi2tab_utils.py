98dfebcc298586706cd4a1ea75d4e4ae1cfcbf0e
maximoskp <maximoskalpap@gmail.com>
2021-11-03 12:41:28 +0200

    corrections to combinations

--- flat_tab_in_CNN/midi2tab_utils.py
+++ flat_tab_in_CNN/midi2tab_utils.py
@@ -17,25 +17,32 @@ def get_midi_full_fretboard():
 
 def make_all_binary_tabs_for_binary_midi(m):
     midi_notes = np.where( m )[0]
-    # print('midi_notes: ', midi_notes)
+    print('midi_notes: ', midi_notes)
     strings_for_notes = []
     frets_for_notes = []
     f = get_midi_full_fretboard()
     for n in midi_notes:
+        # in case n is out of fretboard range
+        while n < np.min(f):
+            n += 12
+            print('n1: ', n)
+        while n > np.max(f):
+            print('n2: ', n)
+            n -= 12
         tmp_where = np.where( n == f )
-        # print('tmp_where: ', tmp_where)
+        print('tmp_where: ', tmp_where)
         strings_for_notes.append( tmp_where[0] )
         frets_for_notes.append( tmp_where[1] )
     # get all combinations of strings
     # print('strings_for_notes: ', strings_for_notes)
     all_combinatios = list( itertools.product( *strings_for_notes ) )
-    # print('all_combinatios: ', all_combinatios)
+    print('all_combinatios: ', all_combinatios)
     # keep only combinations with different elements
     combinations2keep = []
     for c in all_combinatios:
         # check for dublicates
-        if len( list( set( c ) ) ) == len( c ):
-            combinations2keep.append( list(c) )
+        if len( list( set( c ) ) ) not in combinations2keep:
+            combinations2keep.append( list(set( c )) )
     # construct binary fretboard for each string combination
     # print('combinations2keep: ', combinations2keep)
     all_binary_fretboards = []
@@ -57,7 +64,9 @@ def midi_and_flat_tab_decision(m, t):
     # print('t: ', t)
     t_full = np.reshape( t , [6,25] )
     all_binary_tabs = make_all_binary_tabs_for_binary_midi( m )
-    print('all_binary_tabs: ', all_binary_tabs)
+    print('len( all_binary_tabs): ', len(all_binary_tabs))
+    if len(all_binary_tabs) == 0:
+        print(m)
     all_probs = np.zeros( len(all_binary_tabs) )
     for i,b in enumerate( all_binary_tabs ):
         all_probs[i] = np.sum( b*t_full )
